6b751a5312ea378084eeb5bdb2755cac141c2cbe
Ayush Jaiswal <ayush45aj@gmail.com>
2024-11-09 22:54:07 +0530

    issue with uploads folder solved

--- src/middleware/uploadMiddleware.ts
+++ src/middleware/uploadMiddleware.ts
@@ -1,19 +1,25 @@
-import multer from "multer";
-import path from "path";
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+
+// Define the uploads directory path
+const uploadsDir = 'uploads';
+
+// Check if the uploads directory exists; if not, create it
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
 // Configure multer to use disk storage
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, uploadsDir);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname);
     },
 });
+
 const upload = multer({ storage: storage });
-
-
-
 
 export default upload;
